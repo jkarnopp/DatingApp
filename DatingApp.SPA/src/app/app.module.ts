b9f88a26a22fc909d8f1d66442704bd4369ba190
Jim Karnopp <jim@kartech.com>
2018-06-22 13:27:52 -0400

    Auth token update

--- DatingApp.SPA/src/app/app.module.ts
+++ DatingApp.SPA/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { AuthService } from './_services/auth.service';
+import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -5,18 +7,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent
+    ValueComponent,
+    NavComponent
 ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
